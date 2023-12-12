73db73da44e2dfe243a6f3be7c3b186881d5e937
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-12-12 13:34:50 +0200

    slick slider popular ready with adaptive

--- js/js.js
+++ js/js.js
@@ -6,28 +6,38 @@ $('.popular__list').slick({
   slidesToScroll: 5,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1025,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true
+        // dots: true
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+        // dots: true
+      }
+    },
+    {
+      breakpoint: 601,
       settings: {
         slidesToShow: 2,
           slidesToScroll: 2,
-        dots: true
+        // dots: true
         
       }
     },
     {
-      breakpoint: 480,
+      breakpoint: 481,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+          slidesToScroll: 1,
+        // dots: true
       }
     }
     // You can unslick at a given breakpoint now by adding:
